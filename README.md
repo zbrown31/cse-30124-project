69c8337b9a81df5b0d3af8e3d0776898be01fda8
zachbrown <zachary.brown@ridedesi.com>
2023-12-06 00:05:58 -0500

    Add documentation to main and README

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# cse-30124-project
-Class Project for ND CSE 30124: Introduction to Artificial Intelligence; Online Rideshare Distribution
+
+# CSE 30124: Introduction to Artificial Intelligence Final Project
+
+This project seeks to create a simulator to mimic the process of an online rideshare system and develop and evaluate strategies related to the distribution of rides to drivers in a robust and scalable method. 
+
+
+## Authors
+
+- [@zbrown31](https://github.com/zbrown31)
+
+
+
+## Environment Variables
+
+To run this project, you will need to add the following environment variables to your .env file
+
+`GMAPS_API_KEY`
+
+If this value is not added, then the GMapsClient will throw an error of invalid credentials. The author's credentials have been removed to avoid unwanted billing. If it is necessary for testing, the author can provide a temporary key.
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/zbrown31/cse-30124-project
+```
+
+Go to the project directory
+
+```bash
+  cd cse-30124-project
+```
+
+Install dependencies
+
+```bash
+  pip3 install requirements.txt
+```
+
+Edit the main.py file by tweaking the appropriate parameters to configure the program to your specifications
+
+Run the experiments
+
+```bash
+  python3 main.py
+```
+
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Necessary Imports
 from src import DataReader, experiment
 from src.experiment import BatchSizeExperiment, NumDriversExperiment
 from src.utils import GMapsClient
@@ -5,23 +6,59 @@ from src.simulation import NormalCanceller
 from src.simulation.metric import *
 from src.simulation.strategies import *
 
+from typing import Hashable
+
+#Create a GMapsClient object to deal with geocoding, reverse geocode, and distance calculations
+mapper = GMapsClient()
+
+
+#Define the filepath to the backup of the ride requests
+#This file should be in JSON format, preferably as a backup of the Desi Firebase to ensure the fields match
+ride_backup_filepath = "/Users/zachbrown/Desktop/Work/Desi/Tech/Code/firebase_backups/backup_10_30_23.json"
+
+#Read in the data from the file
 data = DataReader(
-    "/Users/zachbrown/Desktop/Work/Desi/Tech/Code/firebase_backups/backup_10_30_23.json"
+    ride_backup_filepath
 )
 
-mapper = GMapsClient()
-# base_strategy = BatchedHungarian(mapper, 3)
-# base_strategy = BaseStrategy()
-
+#Get online drivers from the DataReader
 online_drivers = data.resources["Drivers"]
 
-# results = base_strategy.evaluate(metrics=[MatchRate(), MatchTime(), CancelRate(), RideDistributionByDriver(online_drivers)], rides=list(sorted(data.resources["Rides"], key = lambda x: x.request_time)), drivers=online_drivers)
 
-# [metric.display() for metric in results]
+#Num Drivers Experiment
 
-# experiment = NumDriversExperiment(mapper=mapper, rides=list(sorted(data.resources["Rides"], key = lambda x: x.request_time)), drivers=online_drivers)
-# output = experiment.run([(BaseStrategy, {}),(GreedyStrategy, {}), (HungarianStrategy,{}), (BatchedGreedyStrategy, {'batch_time' : 12}), (BatchedHungarianStrategy, {'batch_time' : 12})], 1, 20)
-# experiment.display()
+#Parameters:
+#Minimum number of drivers to test
+min_num_drivers: int = 1 
+#Maximum number of drivers to test
+max_num_drivers: int = 20 
+#Strategies
+#Pass each strategy in as a tuple 
+#The first element is the type of the strategy
+#The second element is a dictionary mapping the name to the value of each keyword parameter passed to the strategy constructor
+strategies: list[tuple[type, dict[str, Hashable]]] = [(BaseStrategy, {}),(GreedyStrategy, {}), (HungarianStrategy,{}), (BatchedGreedyStrategy, {'batch_time' : 12}), (BatchedHungarianStrategy, {'batch_time' : 12})]
+
+
+experiment = NumDriversExperiment(mapper=mapper, rides=list(sorted(data.resources["Rides"], key = lambda x: x.request_time)), drivers=online_drivers)
+output = experiment.run(strategies, min_num_drivers, max_num_drivers)
+experiment.display()
+
+
+#Batch Size Experiment
+
+#Parameters:
+#Minimum batch size to test
+min_batch_size: int = 1 
+#Maximum batch size to test
+max_batch_size: int = 20 
+#Driver pool size in each test
+num_drivers: int = 5 
+#List of strategies to test
+#Pass each strategy in as a tuple 
+#The first element is the type of the strategy
+#The second element is a dictionary mapping the name to the value of each keyword parameter passed to the strategy constructor
+strategies: list[tuple[type, dict[str,Hashable]]] = [(BatchedGreedyStrategy, {}), (BatchedHungarianStrategy, {})]
+
 
 experiment = BatchSizeExperiment(
     mapper=mapper,
@@ -29,6 +66,6 @@ experiment = BatchSizeExperiment(
     drivers=online_drivers,
 )
 output = experiment.run(
-    [(BatchedGreedyStrategy, {}), (BatchedHungarianStrategy, {})], 1, 1000, 5
+   strategies , min_batch_size, max_batch_size, num_drivers
 )
 experiment.display()
