245a569dcd72ed22d9d32a76a29cc7515ea3390f
zachbrown <zachary.brown@ridedesi.com>
2023-12-06 00:06:19 -0500

    Reformat main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Necessary Imports
+# Necessary Imports
 from src import DataReader, experiment
 from src.experiment import BatchSizeExperiment, NumDriversExperiment
 from src.utils import GMapsClient
@@ -8,56 +8,69 @@ from src.simulation.strategies import *
 
 from typing import Hashable
 
-#Create a GMapsClient object to deal with geocoding, reverse geocode, and distance calculations
+# Create a GMapsClient object to deal with geocoding, reverse geocode, and distance calculations
 mapper = GMapsClient()
 
 
-#Define the filepath to the backup of the ride requests
-#This file should be in JSON format, preferably as a backup of the Desi Firebase to ensure the fields match
-ride_backup_filepath = "/Users/zachbrown/Desktop/Work/Desi/Tech/Code/firebase_backups/backup_10_30_23.json"
-
-#Read in the data from the file
-data = DataReader(
-    ride_backup_filepath
+# Define the filepath to the backup of the ride requests
+# This file should be in JSON format, preferably as a backup of the Desi Firebase to ensure the fields match
+ride_backup_filepath = (
+    "/Users/zachbrown/Desktop/Work/Desi/Tech/Code/firebase_backups/backup_10_30_23.json"
 )
 
-#Get online drivers from the DataReader
+# Read in the data from the file
+data = DataReader(ride_backup_filepath)
+
+# Get online drivers from the DataReader
 online_drivers = data.resources["Drivers"]
 
 
-#Num Drivers Experiment
+# Num Drivers Experiment
 
-#Parameters:
-#Minimum number of drivers to test
-min_num_drivers: int = 1 
-#Maximum number of drivers to test
-max_num_drivers: int = 20 
-#Strategies
-#Pass each strategy in as a tuple 
-#The first element is the type of the strategy
-#The second element is a dictionary mapping the name to the value of each keyword parameter passed to the strategy constructor
-strategies: list[tuple[type, dict[str, Hashable]]] = [(BaseStrategy, {}),(GreedyStrategy, {}), (HungarianStrategy,{}), (BatchedGreedyStrategy, {'batch_time' : 12}), (BatchedHungarianStrategy, {'batch_time' : 12})]
+# Parameters:
+# Minimum number of drivers to test
+min_num_drivers: int = 1
+# Maximum number of drivers to test
+max_num_drivers: int = 20
+# Strategies
+# Pass each strategy in as a tuple
+# The first element is the type of the strategy
+# The second element is a dictionary mapping the name to the value of each keyword parameter passed to the strategy constructor
+strategies: list[tuple[type, dict[str, Hashable]]] = [
+    (BaseStrategy, {}),
+    (GreedyStrategy, {}),
+    (HungarianStrategy, {}),
+    (BatchedGreedyStrategy, {"batch_time": 12}),
+    (BatchedHungarianStrategy, {"batch_time": 12}),
+]
 
 
-experiment = NumDriversExperiment(mapper=mapper, rides=list(sorted(data.resources["Rides"], key = lambda x: x.request_time)), drivers=online_drivers)
+experiment = NumDriversExperiment(
+    mapper=mapper,
+    rides=list(sorted(data.resources["Rides"], key=lambda x: x.request_time)),
+    drivers=online_drivers,
+)
 output = experiment.run(strategies, min_num_drivers, max_num_drivers)
 experiment.display()
 
 
-#Batch Size Experiment
+# Batch Size Experiment
 
-#Parameters:
-#Minimum batch size to test
-min_batch_size: int = 1 
-#Maximum batch size to test
-max_batch_size: int = 20 
-#Driver pool size in each test
-num_drivers: int = 5 
-#List of strategies to test
-#Pass each strategy in as a tuple 
-#The first element is the type of the strategy
-#The second element is a dictionary mapping the name to the value of each keyword parameter passed to the strategy constructor
-strategies: list[tuple[type, dict[str,Hashable]]] = [(BatchedGreedyStrategy, {}), (BatchedHungarianStrategy, {})]
+# Parameters:
+# Minimum batch size to test
+min_batch_size: int = 1
+# Maximum batch size to test
+max_batch_size: int = 20
+# Driver pool size in each test
+num_drivers: int = 5
+# List of strategies to test
+# Pass each strategy in as a tuple
+# The first element is the type of the strategy
+# The second element is a dictionary mapping the name to the value of each keyword parameter passed to the strategy constructor
+strategies: list[tuple[type, dict[str, Hashable]]] = [
+    (BatchedGreedyStrategy, {}),
+    (BatchedHungarianStrategy, {}),
+]
 
 
 experiment = BatchSizeExperiment(
@@ -65,7 +78,5 @@ experiment = BatchSizeExperiment(
     rides=list(sorted(data.resources["Rides"], key=lambda x: x.request_time)),
     drivers=online_drivers,
 )
-output = experiment.run(
-   strategies , min_batch_size, max_batch_size, num_drivers
-)
+output = experiment.run(strategies, min_batch_size, max_batch_size, num_drivers)
 experiment.display()
